49754c12062d434b77faba3d1b5524536436b205
Hari Krishnan <harikrishnanas2002@gmail.com>
2026-01-20 22:07:21 +0530

    solved

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 
 const AppContext = createContext()
 
 
 
 export const AppContextProvider = ({children}) =>{
+
+
+    const navigate = useNavigate()
+    const [user,setUser] = useState(null);
+    const [chats,setChats] = useState([]);
+    const [selectedChats,setSelectedChats] = useState(null);
+    const [theme,setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const fetchUser = async () =>{
+        setUser(dummyUserData)
+    }
+
+    useEffect(() =>{
+        fetchUser()
+    },[])
 
     const value={}
     return(
