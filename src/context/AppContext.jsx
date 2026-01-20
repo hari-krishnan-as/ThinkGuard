e2625b13ffc8d7c904eff6403e4e1e9d76d50e93
Hari Krishnan <harikrishnanas2002@gmail.com>
2026-01-20 21:04:15 +0530

    second commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,28 @@
+import { createContext } from "react";
+
+const AppContext = createContext()
+
+
+
+export const AppContextProvider = ({children}) =>{
+
+
+    const navigate = useNavigate()
+    const [user,setUser] = useState(null);
+    const [chats,setChats] = useState([]);
+    const [selectedChats,setSelectedChats] = useState(null);
+    const [theme,setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const fetchUser = async () =>{
+        setUser(dummyUserData)
+    }
+
+    const value={}
+    return(
+         <AppContext.Provider value={value}>
+            {children}
+         </AppContext.Provider>
+    )
+
+}
+export const useAppContext =()=> useContext(AppContext)
